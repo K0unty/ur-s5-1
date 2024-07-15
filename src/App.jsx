14be51c61289fd60ae234c20e278d84264c8c219
K0unty <kountiapi@pm.me>
2024-07-16 03:28:36 +0530

    setup user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 // Import Setion
 import Header from "./comp/Header";
+import UserInput from "./comp/UserInput";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
 }
 
 export default App;
--- src/comp/UserInput.jsx
+++ src/comp/UserInput.jsx
@@ -1,5 +1,28 @@
 // Fetching the user input
 
 export default function UserInput() {
-  return <section id="user-input"></section>;
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label> Initial PantyStinkSCore</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label> Annual PantyBudget</label>
+          <input type="number" required />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label> Expected Panties</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label> Duration of StinkSmell</label>
+          <input type="number" required />
+        </p>
+      </div>
+    </section>
+  );
 }
