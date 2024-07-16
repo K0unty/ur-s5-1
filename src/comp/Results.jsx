55515b6988f1459b58ad8d60c6d2d7bb6c359c1b
K0unty <kountiapi@pm.me>
2024-07-16 23:52:50 +0530

    fixResults

--- src/comp/Results.jsx
+++ src/comp/Results.jsx
@@ -1,4 +1,4 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 // Component for the table with results
 
@@ -6,5 +6,29 @@ export default function Results({ input }) {
   const resultsData = calculateInvestmentResults(input);
   console.log(resultsData);
 
-  return <p>Pasnties....</p>;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>PantyInvestment</th>
+          <th>PantyIntrest (Year)</th>
+          <th>InverstedPussy</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((yearData) => {
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
