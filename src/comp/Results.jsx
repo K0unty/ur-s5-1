fc35799a452f61e4af9b3a59fff0f1c0789cd673
K0unty <kountiapi@pm.me>
2024-07-16 23:59:22 +0530

    fix results wndow

--- src/comp/Results.jsx
+++ src/comp/Results.jsx
@@ -6,6 +6,11 @@ export default function Results({ input }) {
   const resultsData = calculateInvestmentResults(input);
   console.log(resultsData);
 
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
+
   return (
     <table id="result">
       <thead>
@@ -18,12 +23,17 @@ export default function Results({ input }) {
       </thead>
       <tbody>
         {resultsData.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+
           return (
             <tr key={yearData.year}>
               <td>{yearData.year}</td>
               <td>{formatter.format(yearData.valueEndOfYear)}</td>
-              <td></td>
-              <td></td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
               <td></td>
             </tr>
           );
